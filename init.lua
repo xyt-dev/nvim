3cc5a809783ccbd8dfbce8c8711bcc26d3bbdaf7
xyt-dev <417639959@qq.com>
2024-07-12 11:37:50 +0800

    Add plugins: treesitter rainbow-delimiters autotag Add LSPs: tsserver html cssls

--- init.lua
+++ init.lua
@@ -261,7 +261,9 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
+        html = {},
+        cssls = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -449,6 +451,36 @@ require('lazy').setup({
     end,
   },
   {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    opts = {
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+    config = function(_, opts)
+      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+      -- Prefer git instead of curl in order to improve connectivity in some environments
+      require('nvim-treesitter.install').prefer_git = true
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter.configs').setup(opts)
+
+      -- There are additional nvim-treesitter modules that you can use to interact
+      -- with nvim-treesitter. You should go explore a few and see what interests you:
+      --
+      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    end,
+  },
+  {
     'akinsho/bufferline.nvim',
     dependencies = {
       'nvim-tree/nvim-web-devicons',
@@ -469,32 +501,31 @@ require('lazy').setup({
       vim.keymap.set('n', '<C-t>', ':ToggleTerm direction=float<CR>', { desc = "Toggle terminal" })
     end
   },
-  {
+  { -- indent with '|'
     'lukas-reineke/indent-blankline.nvim',
     -- See `:help ibl`
     main = 'ibl', -- avoid version problem
     opts = {},
   },
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    config = function ()
+      require('rainbow-delimiters.setup').setup {
+        strategy = {
+            -- ...
+        },
+        query = {
+            -- ...
+        },
+        highlight = {
+            -- ...
+        },
+    }
+    end
+  },
+  { -- It works with: astro glimmer handlebars html javascript jsx markdown php rescript svelte tsx twig typescript vue xml
+    'windwp/nvim-ts-autotag', -- < > < /> -- based on treesitter?
+    opts = {},
+  },
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 -- https://github.com/xyt-dev/nvim/
